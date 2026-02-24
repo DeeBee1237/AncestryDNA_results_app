2c839e6143e035af81cada67278a0648c049ac43
Dragos <dragosbercea@gmail.com>
2026-02-24 19:59:40 +1300

    Initial commit (basic set up)

---
+++ AncestryDNAViewPreviousResults/AncestryDNAViewPreviousResultsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AncestryDNAViewPreviousResultsApp.swift
+//  AncestryDNAViewPreviousResults
+//
+//  Created by Joseph on 24/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct AncestryDNAViewPreviousResultsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AncestryDNAViewPreviousResultsTests/AncestryDNAViewPreviousResultsTests.swift
@@ -0,0 +1,17 @@
+//
+//  AncestryDNAViewPreviousResultsTests.swift
+//  AncestryDNAViewPreviousResultsTests
+//
+//  Created by Joseph on 24/02/2026.
+//
+
+import Testing
+@testable import AncestryDNAViewPreviousResults
+
+struct AncestryDNAViewPreviousResultsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
